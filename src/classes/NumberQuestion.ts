27ed1ef78a0018f848402a73ac7e2992ba02c4cf
Alex <alexander.reiprich@hs-furtwangen.de>
2021-11-29 19:39:53 +0100

    change: TextQuestion.ts

--- src/classes/NumberQuestion.ts
+++ src/classes/NumberQuestion.ts
@@ -1,26 +1,29 @@
-class NumberQuestion extends Question{
+class NumberQuestion extends Question {
+  private _questionText: string;
+  private _answers: Array<number> = [];
+  private _correctAnswer: number;
 
-    private _questionText: string;
-    private _answers: Array<number> = [];
-    private _correctAnswer: number;
+  constructor(
+    _questionText: string,
+    _answers: Array<number>,
+    _correctAnswer: number
+  ) {
+    super();
+    this._questionText = _questionText;
+    this._answers = _answers;
+    this._correctAnswer = _correctAnswer;
+  }
 
-    constructor(_questionText: string, _answers: Array<number>, _correctAnswer: number) {
-        super();
-        this._questionText = _questionText;
-        this._answers = _answers;
-        this._correctAnswer = _correctAnswer;
-      }
-    
-    public setAnswers(_answer: number): void{
+  public setAnswers(_answer: number): void {}
 
-    }
+  public setQuestion(_questionText: string): void {}
 
-    public setQuestion(_questionText: string): void{
-
-    }
-
-    public clone(): NumberQuestion {
-        let clonedQuestion: NumberQuestion = new NumberQuestion(this._questionText, this._answers, this._correctAnswer);
-        return clonedQuestion;
-      }
+  public clone(): NumberQuestion {
+    let clonedQuestion: NumberQuestion = new NumberQuestion(
+      this._questionText,
+      this._answers,
+      this._correctAnswer
+    );
+    return clonedQuestion;
+  }
 }
--- src/classes/TextQuestion.ts
+++ src/classes/TextQuestion.ts
@@ -1,26 +1,28 @@
-class TextQuestion  {
-
+class TextQuestion {
   private _questionText: string;
   private _answers: Array<string> = [];
   private _correctAnswer: string;
 
-  constructor(questionText: string, answers: Array<string>, correctAnswer: string) {
+  constructor(
+    questionText: string,
+    answers: Array<string>,
+    correctAnswer: string
+  ) {
     this._questionText = questionText;
     this._answers = answers;
     this._correctAnswer = correctAnswer;
   }
 
-  public setQuestion(questionText: string): void {
-    
-  }
+  public setAnswers(_answer: string): void {}
 
-  public setAnswers(answer: string): void {
-
-  }
+  public setQuestion(_questionText: string): void {}
 
   public clone(): TextQuestion {
-    let clonedQuestion: TextQuestion = new TextQuestion(this._questionText, this._answers, this._correctAnswer);
+    let clonedQuestion: TextQuestion = new TextQuestion(
+      this._questionText,
+      this._answers,
+      this._correctAnswer
+    );
     return clonedQuestion;
   }
-
 }
