77fd909f9ed45935265d2b5b94cd98f56b21ec51
David <david.niemann@hs-furtwangen.de>
2021-11-29 19:32:06 +0100

    change: name, add: console methods

--- src/classes/singletons/FileHandler.ts
+++ src/classes/singletons/FileHandler.ts
@@ -2,28 +2,28 @@ import { readFileSync, writeFileSync } from "fs";
 import { dirname, resolve } from "path";
 
 export class FileHandler {
-  private static _instance : FileHandler = new FileHandler();
+  private static instance : FileHandler = new FileHandler();
   private rootDir : Array<string> = [__dirname, "../../../../"];
 
   private constructor() {
-    if(FileHandler._instance) 
+    if(FileHandler.instance) 
       throw new Error("Instead of using new FileHandler(), please use FileHandler.getInstance() for Singleton!")
-    FileHandler._instance = this;
+    FileHandler.instance = this;
   }
 
   public static getInstance() : FileHandler {
-    return FileHandler._instance;
+    return FileHandler.instance;
   }
 
-  public readJsonFile(pathToFile: string) : any {
-    let jsonRaw : Buffer = readFileSync(resolve(this.rootDir + pathToFile));
+  public readJsonFile(_pathToFile: string) : any {
+    let jsonRaw : Buffer = readFileSync(resolve(this.rootDir + _pathToFile));
     let json : any = JSON.parse(jsonRaw.toString());
 
     return json;
   }
 
-  public writeJsonFile(pathToFile : string, dataToFile: any) : void {
-    writeFileSync(resolve(this.rootDir + pathToFile), JSON.stringify(dataToFile));
+  public writeJsonFile(_pathToFile : string, _dataToFile: any) : void {
+    writeFileSync(resolve(this.rootDir + _pathToFile), JSON.stringify(_dataToFile));
   }
 }
 
