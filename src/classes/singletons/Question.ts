2461815fc5403a4b6799457fbff569e516cb58fe
Laura M <laura.ann-katrin.moser@hs-furtwangen.de>
2021-12-01 16:00:28 +0100

    Question grob

---
+++ src/classes/singletons/Question.ts
@@ -0,0 +1,33 @@
+class Question {
+    public _typeOfQuestion: string;/**typeOfQuestion*/
+    private _questionText: string = "";
+    private _correctAnswer: string = "";
+
+    constructor(
+        typeOfQuestion: string,
+        questionText: string,
+        correctAnswer: string
+      ) {
+        this._typeOfQuestion = typeOfQuestion;
+        this._questionText = questionText;
+        this._correctAnswer = correctAnswer;
+      }
+    
+    public setQuestion(_questionText: string): void {
+
+    }
+    
+    public setAnswers(_answer: string): void {
+
+    }
+
+    public clone(): Question{
+        let clonedQuestion: Question = new Question(
+          this._typeOfQuestion,
+          this._questionText,
+          this._correctAnswer
+        );
+        return clonedQuestion;
+      }
+
+}
